72f25fabf74b6ef3224c01b2547b6098d4f8c088
srinivasrk <srinivasrk92@gmail.com>
2018-05-15 16:21:23 -0400

    changing

--- server/models/maintainers.js
+++ server/models/maintainers.js
@@ -1,7 +1,9 @@
 module.exports = (connection, DataTypes) => {
   const Maintainers = connection.define('maintainers', {
     maintainer:{
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      primaryKey: true,
+      unique: true
     },
   }, {
     timestamps: false,
